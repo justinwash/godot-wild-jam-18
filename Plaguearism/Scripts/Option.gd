b602880096dda2663f45cc0ce713517c15d76fa6
Justin Wash <justin.wash@pm.me>
2020-02-19 19:11:24 -0600

    add detection of correct/incorrect answers

--- Plaguearism/Scripts/Option.gd
+++ Plaguearism/Scripts/Option.gd
@@ -5,8 +5,17 @@ onready var day = $".."
 export(Color) var idle_color = get("custom_colors/font_color")
 export(Color) var hover_color = Color('ff0000')
 
+func _ready():
+	click_area.connect("button_up", self, "_on_click")
+	
 func _physics_process(delta):
 	if (click_area.is_hovered()):
 		add_color_override("font_color", hover_color)
 	else:
 		add_color_override("font_color", idle_color)
+
+func _on_click():
+	if (day.correct_choice_text == self.text):
+		print("correct!")
+	else:
+		print("wrong!")
