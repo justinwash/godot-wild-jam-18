348264fadc5721e020d4462e3454ab856391302a
justinwash <justin.wash@pm.me>
2020-02-22 17:31:19 -0600

    make choices a scene

--- Plaguearism/Scripts/Option.gd
+++ Plaguearism/Scripts/Option.gd
@@ -2,20 +2,27 @@ extends Label
 
 onready var click_area = $ClickArea
 onready var day = $".."
+onready var selected = false
 export(Color) var idle_color = get("custom_colors/font_color")
 export(Color) var hover_color = Color('ff0000')
+export(Color) var correct_color = Color('')
+export(Color) var incorrect_color = Color('')
 
 func _ready():
 	click_area.connect("button_up", self, "_on_click")
 	
 func _physics_process(delta):
-	if (click_area.is_hovered()):
-		add_color_override("font_color", hover_color)
-	else:
-		add_color_override("font_color", idle_color)
+	if !selected:
+		if (click_area.is_hovered()):
+			add_color_override("font_color", hover_color)
+		else:
+			add_color_override("font_color", idle_color)
 
 func _on_click():
+	selected = true
 	if (day.correct_choice_text == self.text):
 		print("correct!")
+		add_color_override("font_color", correct_color)
 	else:
 		print("wrong!")
+		add_color_override("font_color", incorrect_color)
